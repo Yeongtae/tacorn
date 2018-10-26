290bd1bb2a7fa1e9d88c3fb4b90d7c324537e1b5
Yeongtae <hwak1234@hanmail.net>
2018-10-26 13:54:56 +0900

    Implementing backup - model - step

--- wavernn/train.py
+++ wavernn/train.py
@@ -144,7 +144,7 @@ def _train(model, dataset, test_ids, optimiser, epochs, batch_size, classes, seq
             _generate(model=model, step=step, test_ids=test_ids, samples=1)
             # backup
             backup_model = '{}_{}epoch.pyt'.format(MODEL_PATH.split('.')[0], e)
-            backup_step = '{}_{}epoch.npy'.format(STEP_PATH('.')[0], e)
+            backup_step = '{}_{}epoch.npy'.format(STEP_PATH.split('.')[0], e)
             torch.save(model.state_dict(), backup_model)
             np.save(backup_step, [step])
 
